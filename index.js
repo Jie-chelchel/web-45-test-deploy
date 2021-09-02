1b0c8982a5a0cf755a963bbe3c0c2617c7a22834
Jie Zhang <zhangjiew@gmail.com>
2021-09-02 11:08:32 -0600

    done for now

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-const server = require("express")();
+require("dotenv").config(); //this allows you to stash "artificial env variables" in a file
+const express = require("express");
+const cors = require("cors");
+const server = express();
+const port = process.env.PORT || 5000;
+
+server.use(express.json());
+server.use(cors());
+
+// console.log(process.env.USER);
+// console.log(process.env.HOME);
+// console.log(process.env.PORT);
+// console.log(process.env.LADY);
+// console.log(process.env.API_SECRET);
 
 server.get("/", (req, res) => {
-  res.send("heelo");
+  res.send("hello");
 });
 
-server.listen(5000, () => {
-  console.log("listening on port 5000");
+server.get("/api", (req, res) => {
+  res.json({ message: "you rock" });
+});
+
+server.listen(port, () => {
+  console.log(`listenging on ${port}`);
 });
